d1a134e0b91d948e288d0e982357ae9b4904a64e
Ryan Umlas <168735615+Ryowa2@users.noreply.github.com>
2024-05-02 22:02:15 +0800

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,8 @@
+function myPass(){
+    var x = document.getElementById("myInput");
+    if (x.type ==="password") {
+    x.type ="text";
+    }else{
+    x.type ="password";
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,51 @@
+let canvas = document.querySelector("canvas");
+
+let ctx = canvas.getContext("2d");
+let width = canvas.width = window.innerWidth;
+let height = canvas.height = window.innerHeight;
+let str = "A+jk js:2 @dfs 17 tr YY ufds MSr P87 #18 $!& ^dfs $Ew er JH # $ * . (! ;) ,: :";
+let matrix = str.split("");
+let font = 12;
+let col = width / font;
+let arr = [];
+
+for(let i = 0; i < col; i++) {
+    arr[i] = 1;
+}
+
+const draw = () => {
+    ctx.fillStyle = "rgba(0,0,0,0.05)";
+    ctx.fillRect(0, 0, width, height);
+    ctx.fillStyle = "#f70a0e";
+    ctx.font = '${font}px system-ui';
+
+    for(let i = 0; i <arr.length; i++) {
+        let txt = matrix[Math.floor(Math.random() * matrix.length)];
+        ctx.fillText(txt, i * font, arr[i] * font);
+
+        if(arr[i] * font > height && Math.random() > 0.975) {
+            arr[i] = 0;
+        }
+        arr[i]++;
+    }
+}
+setInterval(draw, 20);
+
+window.addEventListener("resize", () => location.reload())
+
+// explore
+const initSlider = () => {
+    const imageList = document.querySelector(".slider .image");
+    const slideButtons = document.querySelectorAll(".slider .slide-button");
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1.1 : 1.1;
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy({left: scrollAmount, behavior: "smooth"});
+        });
+    });
+}
+
+window.addEventListener("load", initSlider);
